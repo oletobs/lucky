50bdeadd3ee3da23afdd7fd5a91b48159dd6f48f
Ole Tobias Strøm <oletobs@stud.ntnu.no>
2016-12-01 13:12:40 +0100

    Add template

---
+++ resources/views/lucky.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>u lucky? @yield('title')</title>
+
+    @section('stylesheets')
+        <link rel="stylesheet" type='text/css' href="http://fonts.googleapis.com/css?family=Lato:400,700|Londrina+Solid">
+        <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700'>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @show
+
+</head>
+
+<body>
+
+@section('header')
+
+@show
+
+@section('content')
+    <p>LYCCCCCKKKKYYYYY</p>
+@show
+
+@section('footer')
+
+@show
+
+@section('scripts')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+@show
+
+</body>
+
+</html>
